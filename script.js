e623cdb738dcf6f2bffcde01ed9deeb80e73cde8
Gokulraj Tamilvindan <gokulraj.tamilvindan@gmail.com>
2025-09-20 23:19:36 +0530

    project done

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  const developers = arr.map((person) => {
+    if (person.profession === "developer") {
+      console.log(person);
+    }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((person) => {
+    if (person.profession === "developer") {
+      console.log(person);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newPerson = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newPerson);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((person) => person.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArray = [
+    { id: 5, name: "alice", age: "22", profession: "designer" },
+    { id: 6, name: "bob", age: "23", profession: "manager" },
+    { id: 7, name: "charlie", age: "21", profession: "developer" },
+  ];
+  const combinedArray = arr.concat(newArray);
+  console.log(combinedArray);
 }
